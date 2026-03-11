16b322fb56e94f8badfb32530442553fe9f75b8c
foyzan <foyzanahmedapon01@gmail.com>
2026-03-11 19:48:58 +0600

    setup a hono backend on Cloudflare Workers template

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,4 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/signup', (c)=> {})
-app.post('/api/v1/signin', (c)=> {})
-app.post('/api/v1/blog', (c)=> {})
-app.put('/api/v1/blog', (c)=> {})
-app.get('/api/v1/blog/:id', (c)=> {})
-app.get('/api/v1/blog', (c)=> {})
-
 export default app
